c00a0dbb5f56c8fa940c89913d1c9f9d93d81d5d
Filip Lenart <287116@student.pwr.edu.pl>
2025-03-15 16:52:21 +0100

    zad2abd

--- Lab1.cpp
+++ Lab1.cpp
@@ -40,11 +40,46 @@ void Z1f(unsigned int n) {
         }
     }
 }
+void Z2a(unsigned int n) {
+    for (int i = n; i <= n and i > 0; i--) {
+        for (int j = n; j <= n and j > 0; j--) {
+            std::cout << j;
+        }
+        std::cout << std::endl;
+    }
+}
+void Z2b(unsigned int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            std::cout << j;
+        }
+        std::cout << std::endl;
+    }
+}
+void Z2c(unsigned int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            std::cout << "3";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void Z2d(unsigned int n) {
+    for (int i = n; i <= n and i>0; i--) {
+        for (int j = i; j <= i and j>0; j--) {
+            std::cout << j;
+        }
+        std::cout << std::endl;
+   }
+}
+
+
 
 int main()
 {
     unsigned int n;
     std::cin >> n;
-    Z1f(n);
+    Z2c(n);
     return 0;
 }
