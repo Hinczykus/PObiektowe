86eb0b2c3f53d0d773f5158fee52fc2558c1dd3b
Filip Lenart <287116@student.pwr.edu.pl>
2025-03-15 16:40:26 +0100

    zad1def

--- Lab1.cpp
+++ Lab1.cpp
@@ -15,13 +15,36 @@ void Z1c(unsigned int n) {
     for (int i = 0; i < n; i++) {
         std::cout << i % 4;
     }
-
+}
+void Z1d(unsigned int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << "0123";
+    }
+}
+void Z1e(unsigned int n) {
+    for (int i = 1; i < n; i++) {
+        std::cout << "0123456789";
+    }
+    std::cout << "0123";
+}
+void Z1f(unsigned int n) {
+    if (n % 2) {
+        for (int i = 0; i < n/2; i++) {
+            std::cout << "37";
+        }
+        std::cout << "3";
+    }
+    else {
+        for (int i = 0; i < n/2; i++) {
+            std::cout << "37";
+        }
+    }
 }
 
 int main()
 {
     unsigned int n;
     std::cin >> n;
-    Z1c(n);
+    Z1f(n);
     return 0;
 }
