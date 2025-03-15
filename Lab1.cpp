ae5f8aa5a3511eaaa706bcf8cd793861779c8f6b
Filip Lenart <287116@student.pwr.edu.pl>
2025-03-15 16:32:35 +0100

    zad1bc

--- Lab1.cpp
+++ Lab1.cpp
@@ -2,14 +2,26 @@
 
 void Z1a(unsigned int n) {
     for (int i = 0; i < n; i++) {
+        std::cout << i % 10;
+    }
+}
+void Z1b(unsigned int n) {
+    for (int i = 0; i < n; i++) {
         std::cout << i % 2;
     }
 }
+void Z1c(unsigned int n) {
+
+    for (int i = 0; i < n; i++) {
+        std::cout << i % 4;
+    }
+
+}
+
 int main()
 {
     unsigned int n;
     std::cin >> n;
-    Z1a(n);
+    Z1c(n);
     return 0;
 }
-
